f2a24a0dafd4908e1b25bc8b9d8e811408a649b2
Jacob Marshall <git@jacobmarshall.co>
2015-12-16 09:33:03 +1300

    Fix the sleep example's export

--- src/js/sleep.jsx
+++ src/js/sleep.jsx
@@ -6,6 +6,6 @@
  * @param {number} ms the number of milliseconds to sleep for
  * @returns {Promise}
  */
-module.exports = function sleep (ms: number) {
+export default async function sleep (ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 };
